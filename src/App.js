5572338f58d9f5f6496b97e193ec7ef24484015b
metaphore520 <119277137+metaphore520@users.noreply.github.com>
2023-01-19 19:32:11 +0600

    Stateless Functional Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ChildComponent from "./components/child";
-import ParentComponent from './components/parent';
+import ParentComponent from "./components/parent";
 
-function App() {
-  return (
-    <React.Fragment>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+class App extends Component {
+  state = {
+    counter: [
+      {
+        id: 1,
+        value: "Rahim",
+      },
+      {
+        id: 2,
+        value: "Kahim",
+      },
+      {
+        id: 3,
+        value: "Mahim",
+      },
+    ],
+    CardTitle: "This is Card Title",
+    CardFooter: "This is Card Footer",
+  };
 
-      <div>
-        <ParentComponent></ParentComponent>
-      </div>
-    </React.Fragment>
-  );
+  constructor() {
+    super();
+    console.log("CONSTRUCTOR PHASE.....................");
+  }
+
+  btnClicked = (childVal) => {
+    let tempCounter = [...this.state.counter];
+    //tempCounter = tempCounter.map((x) => x.value + " -- " + childVal);
+    tempCounter.push({
+      id: Math.floor(Math.random() * 100),
+      value: "Mahim" + childVal,
+    });
+    this.setState({ counter: tempCounter });
+  };
+
+  reset = () => {
+    let tempCounter = [...this.state.counter];
+    tempCounter = tempCounter.map((x) => {
+      x.value = x.value + ".....RESET";
+      console.log("1");
+      return x;
+    });
+    this.setState({ counter: tempCounter });
+  };
+  render() {
+    console.log("RENDER PHASE.....................");
+    return (
+      <React.Fragment>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <ParentComponent
+              data={this.state.counter}
+              addItem={this.btnClicked}
+              resetVal={this.reset}
+              CardTitle={this.state.CardTitle}
+              CardFooter={this.state.CardFooter}
+            ></ParentComponent>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount() {
+    console.log("COMPONENT DID MOUNT PHASE.....................");
+  }
+  componentWillUnmount() {
+    console.log("COMPONENT WIL UN MOUNT PHASE.....................");
+  }
 }
 
 export default App;
