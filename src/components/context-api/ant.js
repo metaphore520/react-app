f5e63a07d6f3c4cb0a4155de7f50a84bb3c6fa39
metaphore520 <119277137+metaphore520@users.noreply.github.com>
2023-01-31 18:20:39 +0600

    Update ant.js

--- src/components/context-api/ant.js
+++ src/components/context-api/ant.js
@@ -4,19 +4,17 @@ import ThemeContext from "./context-all";
 class Ant extends Component {
   state = {};
   render() {
+    const {theme} = this.context;
     return (
       <div>
         <h1>This is a New Component</h1>
-        <ThemeContext.Consumer>
-          {({ theme }) => (
             <div>
               <h1>This is a Ant Component</h1>
               <h1>The given theme is {theme}</h1>
             </div>
-          )}
-        </ThemeContext.Consumer>
       </div>
     );
   }
 }
+Ant.contextType = ThemeContext;
 export default Ant;
